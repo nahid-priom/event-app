87b8e454e778a08ebbe8496e41ca6a1579519bd5
nahid-priom <nahid.priom.06@gmail.com>
2024-02-03 15:35:03 +0600

    Complete Header Section

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -12,8 +13,8 @@ export const metadata: Metadata = {
   title: "MEvents",
   description: "A Platform for event management",
   icons: {
-    icon: '/assets/logo.png'
-  }
+    icon: "/assets/logo.png",
+  },
 };
 
 export default function RootLayout({
@@ -22,8 +23,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={poppins.variable}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={poppins.variable}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
