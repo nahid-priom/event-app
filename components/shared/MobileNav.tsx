ddf588b1ec52b827f9ae1e485e77cbc0170c53c2
nahid-priom <nahid.priom.06@gmail.com>
2024-02-03 15:53:57 +0600

    Footer Section is added

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,42 +1,31 @@
 import {
-    Sheet,
-    SheetContent,
-    SheetDescription,
-    SheetHeader,
-    SheetTitle,
-    SheetTrigger,
-  } from "@/components/ui/sheet"
-  import Image from "next/image"
-  import { Separator } from "../ui/separator"
-  import NavItems from "./NavItems"
-  
-  
-  const MobileNav = () => {
-    return (
-      <nav className="md:hidden">
-        <Sheet>
-          <SheetTrigger className="align-middle">
-            <Image 
-              src="/assets/icons/menu.svg"
-              alt="menu"
-              width={24}
-              height={24}
-              className="cursor-pointer"
-            />
-          </SheetTrigger>
-          <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
-            <Image 
-              src="/assets/images/logo.svg"
-              alt="logo"
-              width={128}
-              height={38}
-            />
-            <Separator className="border border-gray-50" />
-            <NavItems />
-          </SheetContent>
-        </Sheet>
-      </nav>
-    )
-  }
-  
-  export default MobileNav
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import Image from "next/image";
+import { Separator } from "../ui/separator";
+import NavItems from "./NavItems";
+import { MdMenuOpen } from "react-icons/md";
+
+const MobileNav = () => {
+  return (
+    <nav className="md:hidden">
+      <Sheet>
+        <SheetTrigger className="align-middle">
+          <MdMenuOpen size={30} />
+        </SheetTrigger>
+        <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
+          <Image src="/assets/images/logo.png" alt="logo" width={128} height={38} />
+          <Separator className="border border-gray-50" />
+          <NavItems />
+        </SheetContent>
+      </Sheet>
+    </nav>
+  );
+};
+
+export default MobileNav;
